43749a576ac733803e9ec8aa103dc0a615cbba7b
sabina547 <karkisabina226@gmail.com>
2020-05-17 11:59:07 +0545

    Symbols

---
+++ app.js
@@ -0,0 +1,28 @@
+//Create a symbol
+//const sym1 = Symbol();
+//const sym2 = Symbol('sym2');
+
+//console.log(typeof sym2);
+
+//console.log(Symbol('123') === Symbol('123'));
+//console.log(`Hello ${String(sym1)}`);
+
+//Unique Object Keys
+const KEY1 = Symbol();
+const KEy2 = Symbol('sym2');
+
+const myObj = {};
+
+myObj[KEY1] = 'Prop1';
+myObj[KEY2] = 'Prop2';
+myObj.key3 = 'Prop3';
+myObj.key4 = 'Prop4';
+
+console.log(myObj[KEY1]);
+console.log(myObj[KEY2]);
+
+//Symbols are not enumerable in for...in
+for(let i in myObj) {
+    console.log(`${i}: ${myObj[i]}`);
+
+}
